119c972f847a60bf1ba3d78f8f1c4d8ae529972d
Andy Kay <andykay.me@gmail.com>
2024-02-21 09:22:28 -0500

    typescript

--- packages/bot/index.ts
+++ packages/bot/index.ts
@@ -1,8 +1,8 @@
-const startPolling = require("@big-whale-labs/botcaster");
+import { startPolling } from "@big-whale-labs/botcaster";
 
 const fid = 346164;
 
-startPolling.startPolling(
+startPolling(
   fid,
   "3A4B5E7F-95A3-4E80-91A2-D2F1AE5BE306",
   (notification) => {
